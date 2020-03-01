e42e3e4bb812e0db4671c4a13a998a4922ec3b5b
Dmitriy Bezgreshnov <forcenig_91@mail.ru>
2020-03-01 22:24:34 +0300

    Личный проект: начинаем строить сетку на флексах
    Третьи исправления главной страницы.

---
+++ js/popup.js
@@ -0,0 +1,53 @@
+var link = document.querySelector(".button--min");
+
+var popup = document.querySelector(".contact-popup");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+
+  if (storage) {
+    login.value = storage;
+    email.focus();
+  } else {
+    login.focus();
+  }
+});
+
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function(evt) {
+  if (!login.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+    }
+  }
+
+  // if (!login.value) {
+  //   login.style.color = "#fb565a";
+  //   login.style.borderColor = "#fb565a";
+  // }
+});
